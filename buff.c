0c0a46b660805dc34185e6745d6b8c4b66c018ed
Rafaelbeskow <54451428+Rafaelbeskow@users.noreply.github.com>
2019-09-12 13:30:32 -0300

    Update buff.c

--- buff.c
+++ buff.c
@@ -2,43 +2,41 @@
 #include<stdlib.h>
 #include<string.h>
 
-/**
-    A função imprime os valores que estão no buffer passado por parâmetro.
-    Os valores estão intercalados na seguinte ordem: int, float, double.
-    A variável nTotal indica o total de trios (int, float e double) no buffer.
-*/
 
 
 void imprimeAgenda(void *pBuffer,int *i) {
-    for(; *i > 0; *i--) {
-        char *pNome= pBuffer + sizeof(int);
-        int *pTelefone = (pBuffer+50);
-        for(;*pNome!='\0';pNome++){
-        printf("%c",*pNome);
-        }
+    char *pNome=pBuffer;
+    int *pTelefone =  (pBuffer+50);
+    int   *j = i;
+    printf("%d\n",*j);
+    for(; *j> 0;*j--) {
         
-        printf("\n%d\n",*pTelefone);
+        for(;*pNome!='\0';pNome++){
+             printf("%c",*pNome);
+        }
+        *pNome= *pNome + 50*sizeof(char) + sizeof(int) ; 
+            printf("\n%d\n",*pTelefone);
+        *pTelefone= (*pTelefone+100) + sizeof(int); 
     }
+    
 }
 
 void insereAgenda(void *pBuffer,int *i){
-    pBuffer= realloc(pBuffer,*i * (50*sizeof(char) + sizeof (int)));
-    char *pNome=pBuffer+sizeof (int);
-    void *pInicio=pBuffer+sizeof(int);
-    int *pTelefone= (pBuffer+50);
+    *i=*i + 1;
+    pBuffer= realloc(pBuffer,*i * (sizeof (int) + 50*sizeof(char) + sizeof (int)));
+    char *pNome=pBuffer + ((*i - 1) * (50*sizeof(char) + sizeof (int)));
+    int *pTelefone= (pBuffer+50) + ((*i - 1) * (50*sizeof(char) + sizeof (int))) ;
     printf("Digite o nome da pessoa: ");
-        scanf("%[^\n]",pNome);
+    scanf("%[^\n]",pNome);
     printf("Digite o Telefone da pessoa: ");
     scanf("%d",pTelefone);
-    i++;
-    pBuffer = pInicio;
 }
 
 int main () {
     void *pBuffer;
     int *i=pBuffer;
-    *i=1;
-    pBuffer= malloc(sizeof (int) + 50*sizeof(char) + sizeof (int));
+    *i=0;
+    pBuffer= malloc(50*sizeof(char) + sizeof (int));
     insereAgenda(pBuffer,i);
     imprimeAgenda(pBuffer,i);
     insereAgenda(pBuffer,i);
